4c69f29db97fe6d8ec5436ef0734a9ad5310678e
Geneton <gasn1@discente.ifpe.edu.br>
2022-08-23 09:43:40 -0300

    New update POO JAVA

---
+++ Atividades POO/fichas/src/Ficha01/Q3.java
@@ -0,0 +1,34 @@
+/* Escreva um algoritmo em Java para realizar o calculo de area de um trapezio
+retângulo. Todas as entradas do usuário devem estar em cm. A saıda do algoritmo
+deve ser a  ́area do trapezio em cm2 no console; 
+(e.g. ”A  ́area do trapezio em cm2  ́e ”).
+ */
+
+
+package Ficha01;
+
+import java.util.Scanner;
+
+public class Q3 {
+
+	public static void main(String[] args) {
+
+Scanner in = new Scanner (System.in);
+		
+		double area, baseMaior, baseMenor, altura; 
+		
+		System.out.println("Digite o valor da Base Maior");
+		baseMaior = in.nextDouble();
+		
+		System.out.println("Digito o valor da Base Menor");
+		baseMenor = in.nextDouble();
+		
+		System.out.println("Digite o valor da Altura");
+		altura = in.nextDouble();
+		
+		area = (baseMaior + baseMenor) * altura / 2;
+		System.out.println(area);
+		
+	}
+
+}
