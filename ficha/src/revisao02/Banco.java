0d249f107c7a59eecd7b01ccfd5024d3a914320d
Geneton <gasn1@discente.ifpe.edu.br>
2022-09-29 22:24:08 -0300

    revisao sobre o metodo get

---
+++ ficha/src/revisao02/Banco.java
@@ -0,0 +1,35 @@
+package revisao02;
+
+public class Banco {
+
+	public static void main(String[] args) {
+
+		Cliente c1 = new Cliente();
+		c1.nome = "Beatriz";
+		c1.saldo = 1500.00f;
+		c1.nConta = 6066;
+		c1.nAgencia = 12345.6f;
+		c1.banco = "Itau";
+		
+		Cliente c2 = new Cliente("Ana", 0f);
+		c2.nConta = 1234;
+		c2.nAgencia = 98765.4f;
+		c2.banco = "Bradesco";
+		
+		Cliente c3 = new Cliente(7363, 32459.3f);
+		c3.nome = "Maria";
+		c3.saldo = 800.80f;
+		c3.banco = "NuBank";
+		
+		//Cliente c4 = c3;
+		
+		//System.out.println(c1.toString());
+		//System.out.println(c2.toString());
+		//System.out.println(c3.toString());
+		
+		//METODO EQUALS
+		System.out.println(c1.equals(c1));
+		//System.out.println(c2.equals(c4));
+	}
+
+}
---
+++ ficha/src/revisao02/Cliente.java
@@ -0,0 +1,38 @@
+package revisao02;
+
+public class Cliente {
+
+	public String nome;
+	protected float saldo;
+	protected int nConta;
+	protected float nAgencia;
+	public String banco;
+	
+	Cliente(){
+		
+	}
+	
+	Cliente(String nome, float saldo){
+		this.nome = nome;
+		this.saldo = saldo;
+	}
+	
+	Cliente(int nConta, float nAgencia){
+		this.nConta = nConta;
+		this.nAgencia = nAgencia;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "Nome: " + nome + " Saldo: " + saldo + " Nº Conta: " + nConta + " Nº Agencia: " + nAgencia + " Banco: " + banco;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		Cliente c = (Cliente) obj;
+		return this.saldo == c.saldo;
+	}
+	
+	
+}
