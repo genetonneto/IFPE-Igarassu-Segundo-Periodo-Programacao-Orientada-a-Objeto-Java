467aa68bfa90c2083f1f503b8f26b3949a464c6f
Geneton <gasn1@discente.ifpe.edu.br>
2022-10-18 22:16:42 -0300

    refazendo a prova da unidade I questao II

--- ficha/src/avaliaçaoq2/Banco.java
+++ ficha/src/avaliaçaoq2/Banco.java
@@ -2,7 +2,7 @@ package avaliaçaoq2;
 public class Banco {
 
 	public static void main(String[] args) {
-		Conta c1 = new Conta("Itaú", 123.4f, 0f);
+		Conta c1 = new Conta("Itaú");
 		System.out.println(c1);
 	}
 
--- ficha/src/avaliaçaoq2/Conta.java
+++ ficha/src/avaliaçaoq2/Conta.java
@@ -3,74 +3,60 @@ import java.util.Objects;
 
 public class Conta {
 	String nomeBanco;
-	float conta;
+	float numeroConta;
 	float saldo;
-	boolean ativada;
-	boolean desativada;
+	boolean status;
 	
 	
-	public Conta(String nomeBanco, float conta, float saldo) {
+	public Conta(String nomeBanco) {
 		this.nomeBanco = nomeBanco;
-		this.conta = conta;
-		this.saldo = saldo;
-		this.status();
-		this.contaAtivada();
-		this.consultarSaldo();
-		this.sacar();
-		this.transferir();	
+		this.numeroConta = 1234.5f;
+		this.saldo = 1f;
+		this.status = true;
 	}
 	
 	public Conta() {
 		
 	}
 	
-	@Override
-	public String toString() {
-		return "Conta [nomeBanco=" + nomeBanco + ", conta=" + conta + ", saldo=" + saldo + ", ativada=" + ativada
-				+ ", desativada=" + desativada + "]";
-	}
-	
-	public void status() {
-		
-	}
-	
-	
 	public void contaAtivada() {
-		this.ativada = true;
+		this.status = true;
 	}
 	
 	public void contaDesativada() {
-		this.desativada = false;
+		this.status = false;
 	}
 	
 	public void consultarSaldo() {
-		if(this.ativada == true && this.desativada == false) {
+		if(this.status == true) {
 			System.out.println("Seu saldo atual é " + this.saldo);
 		} else {
 			System.out.println("ERRO! Sua conta precisa estar ativada");
 		}
 	}
 
-	public void sacar() {
-		if(this.saldo >= 0 && this.ativada == true && this.desativada == false) {
-			System.out.println("Retire o dinheiro na boca do caixa");
-		} else {
-			System.out.println("Impossivel realizar saque");
+	public boolean sacar(float quantia) {
+		if(quantia > 0 && this.saldo <= quantia && this.status == true) {
+			this.saldo -= quantia;
+			return true;
+		}else {
+			return false;
 		}
 	}
 	
-	public void transferir() {
-		if(this.saldo >= 0 && this.ativada == true && this.desativada == false) {
-			System.out.println("Transação autorizada");
-		} else {
-			System.out.println("Transação não atorizada");
+	public void transferir(float quantia, Conta destino) {
+		if(quantia > 0 && this.saldo >= quantia && destino.status && this.status) {
+			this.saldo -= quantia;
+			destino.saldo += quantia;
+			System.out.println("Tudo ok");
+		}else {
+			System.out.println("Erro ! A transferencia não pode ser realizada!");
 		}
 	}
-	
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(ativada, conta, desativada, nomeBanco, saldo);
+		return Objects.hash(numeroConta);
 	}
 
 	@Override
@@ -82,11 +68,12 @@ public class Conta {
 		if (getClass() != obj.getClass())
 			return false;
 		Conta other = (Conta) obj;
-		return ativada == other.ativada && Float.floatToIntBits(conta) == Float.floatToIntBits(other.conta)
-				&& desativada == other.desativada && Objects.equals(nomeBanco, other.nomeBanco)
-				&& Float.floatToIntBits(saldo) == Float.floatToIntBits(other.saldo);
+		return Float.floatToIntBits(numeroConta) == Float.floatToIntBits(other.numeroConta);
 	}
-}
 
-	
-	
+	@Override
+	public String toString() {
+		return "Conta [nomeBanco=" + nomeBanco + ", numeroConta=" + numeroConta + ", saldo=" + saldo + ", status="
+				+ status + "]";
+	}	
+}
